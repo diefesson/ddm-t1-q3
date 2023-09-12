dd97eb4c73e34d9da3bb6474aa5284b128c6d7d4
diefesson <diefesson.so@gmail.com>
2023-09-11 22:52:40 -0300

    impl q3

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={[styles.square, styles.red]}>
+        <Text>Square 1</Text>
+      </View>
+      <View style={[styles.square, styles.green]}>
+        <Text>Square 2</Text>
+      </View>
+      <View style={[styles.square, styles.blue]}>
+        <Text>Square 3</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  square: {
+    width: 100,
+    height: 100,
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  red: {
+    backgroundColor: '#eb4034',
+  },
+  green: {
+    backgroundColor: '#34eb8c',
+  },
+  blue: {
+    backgroundColor: '#34b4eb',
   },
 });
